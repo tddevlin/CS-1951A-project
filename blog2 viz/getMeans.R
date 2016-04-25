0745754b7fef5f5c00480917487f51f4f1feba0b
tylerdevlin <tylerdevlin@me.com>
2016-04-25 17:36:13 -0400

    minor fixes
    Eliminate redundancy between “C” and “OC”. Reorder positions to make
    more sense.

--- blog2 viz/getMeans.R
+++ blog2 viz/getMeans.R
@@ -6,20 +6,26 @@ minVal = 0
 normalize = function(x) {
     (x - min(x, na.rm = T))/(max(x, na.rm = T) - min(x, na.rm = T))
 }
-
+# invert scores where less => better
 invertTimes = function(x) { 1 - x }
+# desired viz order
+ordering = c("QB","WR","RB","FB",
+             "OT","OG","C","TE",
+             "OLB","ILB","DE","DT",
+             "CB","SS","FS","LS",
+             "P","K")
 
 newdat = dat %>%
     group_by(Pos) %>%
     select(Yd40:Shuttle) %>%
     summarize_each(funs(mean(., na.rm = T))) %>%
     mutate_each(funs(normalize), Yd40:Shuttle) %>%
-    mutate_each(funs(invertTimes), c(Yd40, Cone3, Shuttle))
+    mutate_each(funs(invertTimes), c(Yd40, Cone3, Shuttle)) %>%
+    slice(match(ordering, Pos))
 
 # set NaNs to 0
-newdat[8,6:7] = c(minVal,minVal)
-newdat[10,6] = minVal
-newdat[14,6:7] = c(minVal,minVal)
+newdat[17,6:7] = c(minVal,minVal)
+newdat[18,6:7] = c(minVal,minVal)
 
 write.csv(newdat, "summarizedData.csv", row.names = F)
 
